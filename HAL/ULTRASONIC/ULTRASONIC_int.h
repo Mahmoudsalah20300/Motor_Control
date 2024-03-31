8facb7848232aa994c9e1d33bf240a8019c18d31
Mahmoudsalah20300 <Mahmoudsalah20300@gmail.com>
2024-03-31 18:36:52 +0200

    This is the Layer where Hardware Layer like LEDs,.....

---
+++ HAL/ULTRASONIC/ULTRASONIC_int.h
@@ -0,0 +1,17 @@
+/*
+ * ULTRASONIC_int.h
+ *
+ *  Created on: Mar 2, 2023
+ *      Author: Mahmoud_Ali
+ */
+
+#ifndef ULTRASONIC_INT_H_
+#define ULTRASONIC_INT_H_
+
+void HUltrasonic_voidinit(void);
+
+u16 HULTRASONIC_u16GetDistance(void);
+
+void Ultrasonic_edgeProcessing(void);
+
+#endif /* ULTRASONIC_INT_H_ */
---
+++ HAL/ULTRASONIC/ULTRASONIC_priv.h
@@ -0,0 +1,13 @@
+/*
+ * ULTRASONIC_priv.h
+ *
+ *  Created on: Mar 2, 2023
+ *      Author: Mahmoud_Ali
+ */
+
+#ifndef ULTRASONIC_PRIV_H_
+#define ULTRASONIC_PRIV_H_
+
+#define TRIGGER_PULSE_DURATION    50
+
+#endif /* ULTRASONIC_PRIV_H_ */
